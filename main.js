41765f6864bfd5ee0db35c1df64f61042e14a13f
vasani2232-alt <vasani2232@.in>
2026-02-10 20:41:36 +0530

    Update portfolio website

---
+++ main.js
@@ -0,0 +1,17 @@
+var typed = new Typed(".text" ,{
+    strings:["Frontend Developer" , "Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+// window.onload = function () {
+//     new Typed(".text", {
+//         strings: ["Frontend Developer", "Youtuber", "Web Developer"],
+//         typeSpeed: 100,
+//         backSpeed: 100,
+//         backDelay: 1000,
+//         loop: true
+//     });
+// };
